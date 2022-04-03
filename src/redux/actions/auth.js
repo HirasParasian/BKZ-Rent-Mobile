c8a871ec62822c8e98f4114d9953cd54d121ef6c
HirasParasian <hirasparasian@gmail.com>
2022-04-03 13:30:11 +0700

    Feat : Signup Intergration

--- src/redux/actions/auth.js
+++ src/redux/actions/auth.js
@@ -27,3 +27,38 @@ export const loginProcess = (username, password) => {
     }
   };
 };
+
+export const OnRegister = (username, password, MobileNumber) => {
+  const dataa = {
+    username: username,
+    password: password,
+    MobileNumber: MobileNumber,
+  };
+  return async dispatch => {
+    try {
+      dispatch({
+        type: 'CLEAR_ERROR',
+      });
+      const { data } = await http().post(
+        '/users/register',
+        qs.stringify(dataa),
+      );
+      console.log(data);
+      dispatch({
+        type: 'AUTH_SIGNUP',
+        payload: data.results,
+      });
+    } catch (err) {
+      let payload = '';
+      if (err.response) {
+        payload = err.response.data.message;
+      } else {
+        payload = err.message;
+      }
+      dispatch({
+        type: 'AUTH_ERROR',
+        payload: payload,
+      });
+    }
+  };
+};
