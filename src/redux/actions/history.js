d5152f46d9ad02d3ab0e0a5c7a5ac4a760e3c6d2
HirasParasian <hirasparasian@gmail.com>
2022-04-09 22:59:29 +0700

    Feat : Delete History & Add Empty History

--- src/redux/actions/history.js
+++ src/redux/actions/history.js
@@ -3,10 +3,30 @@ import qs from 'qs';
 
 export const getMyHistory = (token, page) => {
   return async dispatch => {
+    dispatch({
+      type: 'CLEAR_ERROR_HISTORY',
+    });
     try {
       const { data } = await http(token).get(`/myHistory?page=${page}`);
       dispatch({
         type: 'GET_MY_HISTORY',
+        payload: data,
+      });
+    } catch (e) {
+      dispatch({
+        type: 'HISTORY_ERROR',
+        payload: e.response.data.message,
+      });
+    }
+  };
+};
+
+export const deleteHistory = idHistory => {
+  return async dispatch => {
+    try {
+      const { data } = await http().delete(`/myHistory/${idHistory}`);
+      dispatch({
+        type: 'DELETE_HISTORY',
         payload: data,
       });
     } catch (e) {
--- src/redux/reducers/history.js
+++ src/redux/reducers/history.js
@@ -5,6 +5,7 @@ const initialState = {
   myHistory: [],
   historyPage: {},
   successMsg: '',
+  delete: '',
 };
 const history = (state = initialState, action) => {
   switch (action.type) {
@@ -14,7 +15,20 @@ const history = (state = initialState, action) => {
     case 'GET_MY_HISTORY': {
       state.myHistory = action.payload.results;
       state.historyPage = action.payload.pageInfo;
+      state.successMsg = action.payload.message;
       return { ...state };
+    }
+    case 'DELETE_HISTORY': {
+      state.delete = action.payload.message;
+      return { ...state };
+    }
+    case 'CLEAR_HISTORY': {
+      state.myHistory = [];
+      state.historyPage = {};
+      return { ...state };
+    }
+    case 'HISTORY_ERROR': {
+      return { ...state, isError: true, errMsg: action.payload };
     }
     default: {
       return { ...state };
